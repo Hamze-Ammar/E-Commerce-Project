b423ff91f6dd14c04a260de21c920daa2fd21756
Hamze-A <99362310+Hamze-A@users.noreply.github.com>
2022-06-12 09:30:54 +0300

    Added register user api | frontend user

--- frontend-user/javascript/main.js
+++ frontend-user/javascript/main.js
@@ -7,6 +7,9 @@ var login_btn = document.getElementById("login");
 // Get the <span> element that closes the modal
 var login_span = document.getElementsByClassName("close")[0];
 
+//Get msg modal
+var msg_modal = document.getElementById("msg-modal");
+
 // When the user clicks the button, open the modal 
 login_btn.onclick = function() {
     login_modal.style.display = "block";
@@ -48,5 +51,82 @@ register_span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == register_modal) {
     register_modal.style.display = "none";
+  }
+}
+
+// Sign Up =========================
+// validate email address function
+function validateEmail (emailAddress){
+  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (emailAddress.match(regexEmail)) {
+    return true; 
+  } else {
+    return false; 
+  }
+}
+function validatePassword(pass){
+  if (pass.length<6){
+    return false;
+  }
+  return true;
+}
+
+let register_submit_btn = document.getElementById("sign-up-submit");
+register_submit_btn.onclick = function(){
+  let name = document.getElementById("name-signup").value;
+  let email = document.getElementById("email-signup").value;
+  let password = document.getElementById("password-signup").value;
+  let password_confirmation = document.getElementById("password_confirmation-signup").value;
+  document.getElementById("msg-signup").innerHTML = "";
+  if (!name || !email || !password){
+    document.getElementById("msg-signup").innerHTML = "Missing values! All fields are required";
+    return
+  }
+  console.log(validateEmail(email));
+  if (!validateEmail(email)){
+    document.getElementById("msg-signup").innerHTML = "Invalid email Address!";
+  }
+  if (!validatePassword(password)){
+    document.getElementById("msg-signup").innerHTML = "Password should be at least of 6 characters!";
+  }
+  if (password !== password_confirmation){
+    document.getElementById("msg-signup").innerHTML = "The password confirmation does not match.";
+  }
+  //Axios
+  let my_url = 'http://127.0.0.1:8000/api/v1/user/register';
+  let data = new FormData();
+  data.append('name', name);
+  data.append('email', email);
+  data.append('password', password);
+  data.append('password_confirmation', password_confirmation);
+  axios({
+  method: "post",
+  url: my_url,
+  data: data,
+
+  }).then(function (response) {
+      handleRegisterResponse(response);
+  }).catch(function (error) {
+      if (error.response) {
+        // Request made and server responded
+        if(error.response.data.email){
+          alert(error.response.data.email[0]);
+        }
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+    }})
+}
+
+function handleRegisterResponse(response){
+  console.log(response.data.message);
+  if (response.data.message=='User successfully registered'){
+    register_modal.style.display = "none";
+    msg_modal.style.display = "block";
   }
 }
